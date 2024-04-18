a46b94701469f12bccca5c538c0dd30300e4484c
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-04-18 11:22:27 -0300

    criado o componente Main para centralizar todos componentes filhos

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,42 +1,48 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    :root {
-        --background: #FFFFFF;
-        --color-header: #000000;
-        --backgroud-sencodary: #FFF3E3;
+  :root {
+    --background: #FFFFFF;
+    --color-header: #000000;
+    --backgroud-sencodary: #FFF3E3;
 
-    }
+  }
     
-    *,
-    *:after,
-    *:before {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+  *,
+  *:after,
+  *:before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
     }
 
-    html {
-        @media (max-width: 1080px) {
-            font-size: 93.75%;
-        }
-
-        @media (max-width: 720px) {
-            font-size: 87.5%;
-        }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
     }
+  }
     
-    body {
-        background-color: var(--background);
-        --webkit-font-smoothing: antialiased;
-    }
+  body {
+    background-color: var(--background);
+    --webkit-font-smoothing: antialiased;
+  }
 
-    body, input, button {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
-    }
+  body, input, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
 
-    button {
-        cursor: pointer;
-    }
+  button {
+    cursor: pointer;
+  }
+`;
+
+export const Main = styled.main`
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 1rem;
 `;
