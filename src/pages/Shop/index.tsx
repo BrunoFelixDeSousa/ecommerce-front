afd7d06377643124ce183b8f57a2456df0e1a8df
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-04-29 02:33:59 -0300

    page shop funcional

--- src/pages/Shop/index.tsx
+++ src/pages/Shop/index.tsx
@@ -1,4 +1,4 @@
-import { BannerShop } from "../../components/BannerShop"; 
+import { BannerShop } from "../../components/BannerShop";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Info } from "../../components/Info";
