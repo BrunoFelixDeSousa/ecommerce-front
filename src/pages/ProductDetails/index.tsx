0a6f53107ca45f54d5f6b0b97a08e393a233617f
Bruno Felix <brunofelixdesousa@hotmail.com>
2024-04-29 11:12:19 -0300

    Estilizando pagina ProductDetails

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,18 +1,206 @@
 import { useParams } from "react-router-dom";
 import { Header } from "../../components/Header";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import {
+  Container,
+  ContentPage,
+  ContentPageHome,
+  ContentPages,
+  ImageWrapper,
+  InfoContainer,
+  LargeImage,
+  NameProduct,
+  ProductContainer,
+  ProductContent,
+  ProductImages,
+  ProductInfo,
+  SmallImage,
+  Size,
+  Color,
+  ButtonColor,
+  ButtonSize,
+  ProductButtons,
+  ButtonQtd,
+  ButtonAddCart,
+  ButtonCompare,
+  ButtonMinus,
+  ButtonPlus,
+  ProductMoreInfo,
+  Info,
+  ProductDescription,
+  DescriptionTitle,
+  AddInformationTitle,
+  Description,
+} from "./styles";
+import {
+  RiArrowRightSLine,
+  RiFacebookCircleFill,
+  RiLinkedinBoxFill,
+  RiStarFill,
+  RiStarHalfFill,
+  RiTwitterXFill,
+} from "@remixicon/react";
+import { Footer } from "../../components/Footer";
+
+interface Product {
+  id: string;
+  name: string;
+  sku: string;
+  categoryId: string;
+  description: string;
+  largeDescription: string;
+  price: number;
+  discountPercent: number;
+  isNew: boolean;
+  imageLink: string;
+  otherImagesLink: string[];
+}
 
 export function ProductDetails() {
   const { id } = useParams();
 
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      try {
+        const response = await api.get(`/product/${id}`);
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    }
+
+    fetchProduct();
+  }, [id]);
+
   return (
     <>
       <Header />
-      <div>
-        <h2>Product Details</h2>
-        <p>Product ID: {id}</p>
-        {/* Exiba os detalhes do produto aqui */}
-      </div>
+      <InfoContainer>
+        <ContentPage>
+          <ContentPageHome to="/">Home</ContentPageHome>
+          <RiArrowRightSLine />
+          <ContentPages to="/shop">Shop</ContentPages>
+          <RiArrowRightSLine />
+          <NameProduct>{product?.name}</NameProduct>
+        </ContentPage>
+      </InfoContainer>
+      <Container>
+        <ProductContainer>
+          <ProductImages>
+            <ImageWrapper>
+              {product?.otherImagesLink.map((link, index) => (
+                <SmallImage src={link} key={index} />
+              ))}
+            </ImageWrapper>
+            <LargeImage src={product?.imageLink} alt="Large Image" />
+          </ProductImages>
+          <ProductInfo>
+            {product && (
+              <ProductContent>
+                <h1>{product?.name}</h1>
+                <span>R$ {product.price}</span>
+                <ul>
+                  <li>
+                    <RiStarFill color="#FFC700" />
+                  </li>
+                  <li>
+                    <RiStarFill color="#FFC700" />
+                  </li>
+                  <li>
+                    <RiStarFill color="#FFC700" />
+                  </li>
+                  <li>
+                    <RiStarFill color="#FFC700" />
+                  </li>
+                  <li>
+                    <RiStarHalfFill color="#FFC700" />
+                  </li>
+                  <li>
+                    <div>5 Customer Review</div>
+                  </li>
+                </ul>
+                <p>{product.largeDescription}</p>
+
+                <Size>
+                  Size
+                  <ul>
+                    <li>
+                      <ButtonSize color="#B88E2F">L</ButtonSize>
+                    </li>
+                    <li>
+                      <ButtonSize color="#F9F1E7">XL</ButtonSize>
+                    </li>
+                    <li>
+                      <ButtonSize color="#F9F1E7">XS</ButtonSize>
+                    </li>
+                  </ul>
+                </Size>
+                <Color>
+                  Color
+                  <ul>
+                    <li>
+                      <ButtonColor color="#9747ff"></ButtonColor>
+                    </li>
+                    <li>
+                      <ButtonColor color="#121212"></ButtonColor>
+                    </li>
+                    <li>
+                      <ButtonColor color="#B88E2F"></ButtonColor>
+                    </li>
+                  </ul>
+                </Color>
+
+                <ProductButtons>
+                  <ButtonQtd>
+                    <ButtonMinus>-</ButtonMinus>
+                    <span>1</span>
+                    <ButtonPlus>+</ButtonPlus>
+                  </ButtonQtd>
+                  <ButtonAddCart>Add To Cart</ButtonAddCart>
+                  <ButtonCompare>+ Compare</ButtonCompare>
+                </ProductButtons>
+
+                <ProductMoreInfo>
+                  <Info>SKU : {product.sku}</Info>
+                  <Info>Category : {product.categoryId}</Info>
+                  <Info>
+                    Share :
+                    <RiFacebookCircleFill size={20} color="#000000" />
+                    <RiLinkedinBoxFill size={20} color="#000000" />
+                    <RiTwitterXFill size={20} color="#000000" />
+                  </Info>
+                </ProductMoreInfo>
+              </ProductContent>
+            )}
+          </ProductInfo>
+        </ProductContainer>
+      </Container>
+      <ProductDescription>
+        <div>
+          <DescriptionTitle>Description</DescriptionTitle>
+          <AddInformationTitle>Additional Information</AddInformationTitle>
+        </div>
+
+        <Description>
+          Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn
+          portable active stereo speaker takes the unmistakable look and sound
+          of Marshall, unplugs the chords, and takes the show on the road.
+        </Description>
+        <Description>
+          Weighing in under 7 pounds, the Kilburn is a lightweight piece of
+          vintage styled engineering. Setting the bar as one of the loudest
+          speakers in its class, the Kilburn is a compact, stout-hearted hero
+          with a well-balanced audio which boasts a clear midrange and extended
+          highs for a sound that is both articulate and pronounced. The analogue
+          knobs allow you to fine tune the controls to your personal preferences
+          while the guitar-influenced leather strap enables easy and stylish
+          travel.
+        </Description>
+      </ProductDescription>
+      <Footer />
     </>
   );
 }
-
---
+++ src/pages/ProductDetails/styles.ts
@@ -0,0 +1,309 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const InfoContainer = styled.div`
+  max-width: 100%;
+  height: 6rem;
+  padding-left: 9rem;
+
+  display: flex;
+  align-items: center;
+
+  background-color: var(--backgroud-sencodary);
+
+  @media (max-width: 768px) {
+    height: 5.5rem;
+
+    padding-left: 1rem;
+  }
+`;
+
+export const NameProduct = styled.span`
+  font-size: 1rem;
+  font-weight: 400;
+  padding-left: 2rem;
+  margin-left: 1rem;
+
+  border-left: 2px solid var(--color-text-4);
+`;
+
+export const ContentPage = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+`;
+
+export const ContentPageHome = styled(Link)`
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--color-text-4);
+`;
+
+export const ContentPages = styled(Link)`
+  font-size: 1rem;
+  font-weight: 400;
+  color: var(--color-text-4);
+`;
+
+export const Container = styled.div`
+  max-width: 90rem;
+  height: 51rem;
+  margin: 0 auto;
+
+  @media (max-width: 1000px) {
+    height: 82rem;
+  }
+`;
+
+export const ProductContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 4rem;
+  margin: 2rem auto;
+  justify-content: center;
+  /* flex-wrap: wrap; */
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+`;
+
+export const ProductImages = styled.div`
+  width: 34rem;
+  height: 31.25rem;
+
+  display: flex;
+  gap: 0.5rem;
+  padding: 0 1rem;
+  justify-content: center;
+  align-items: flex-start;
+
+  @media (max-width: 768px) {
+    gap: 1rem;
+    width: 27.5rem;
+    height: 31.25rem;
+  }
+`;
+
+export const ProductInfo = styled.div`
+  width: 37.875rem;
+  height: 45.625rem;
+
+  @media (max-width: 768px) {
+    gap: 1rem;
+    width: 35.5rem;
+    height: 35.25rem;
+  }
+`;
+
+export const ProductContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  h1 {
+    font-size: 2.625rem;
+    font-weight: 400;
+  }
+
+  span {
+    color: var(--color-text-4);
+    font-size: 1.5rem;
+    font-weight: 500;
+  }
+
+  ul {
+    display: flex;
+    li {
+      margin-right: 0.5rem;
+
+      div {
+        border-left: 2px solid #9f9f9f;
+        padding-left: 2rem;
+        margin-left: 1rem;
+        font-size: 0.813rem;
+        font-weight: 400;
+      }
+    }
+  }
+
+  p {
+    width: 424px;
+    font-size: 0.813rem;
+    font-weight: 400;
+    line-height: 1.3rem;
+  }
+`;
+
+export const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const SmallImage = styled.img`
+  width: 4.75rem;
+  height: 5rem;
+  border-radius: 10px;
+`;
+
+export const LargeImage = styled.img`
+  width: 26.438rem;
+  height: 30.25rem;
+  border-radius: 10px;
+`;
+
+export const Size = styled.div`
+  color: var(--color-text-4);
+  font-size: 1rem;
+  margin-top: 0.6rem;
+  margin-bottom: 0.3rem;
+
+  ul {
+    li {
+      margin-top: 0.5rem;
+      margin-right: 1rem;
+    }
+  }
+`;
+
+export const Color = styled.div`
+  color: var(--color-text-4);
+  font-size: 1rem;
+
+  ul {
+    li {
+      margin-top: 0.5rem;
+      margin-right: 1rem;
+    }
+  }
+`;
+
+export const ButtonColor = styled.button`
+  width: 1.875rem;
+  height: 1.875rem;
+  border-radius: 50%;
+  border: none;
+  background-color: ${(props) => props.color};
+`;
+
+export const ButtonSize = styled.button`
+  width: 1.875rem;
+  height: 1.875rem;
+  border-radius: 5px;
+  border: none;
+  background-color: ${(props) => props.color};
+  color: black;
+`;
+
+export const ProductButtons = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const ButtonQtd = styled.div`
+  width: 7.688rem;
+  height: 4rem;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  border: 1px solid #9f9f9f;
+  border-radius: 5px;
+
+  span {
+    color: #000000;
+  }
+`;
+
+export const ButtonMinus = styled.button`
+  font-size: 1rem;
+  font-weight: 500;
+  border: none;
+  background-color: transparent;
+`;
+
+export const ButtonPlus = styled.button`
+  font-size: 1rem;
+  font-weight: 500;
+  border: none;
+  background-color: transparent;
+`;
+
+export const ButtonAddCart = styled.button`
+  width: 13.438rem;
+  height: 4rem;
+  font-size: 1rem;
+  font-weight: 500;
+  border: 1px solid #000000;
+  border-radius: 5px;
+  background-color: transparent;
+`;
+
+export const ButtonCompare = styled.button`
+  width: 13.438rem;
+  height: 4rem;
+  font-size: 1rem;
+  font-weight: 500;
+  border: 1px solid #000000;
+  border-radius: 5px;
+  background-color: transparent;
+`;
+
+export const ProductMoreInfo = styled.div`
+  margin-top: 4.25rem;
+  padding-top: 2rem;
+  border-top: 1px solid #9f9f9f;
+  /* border-bottom: 3rem; */
+`;
+
+export const Info = styled.p`
+  font-size: 1rem;
+  font-weight: 400;
+  color: #9f9f9f;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ProductDescription = styled.div`
+  width: 100%;
+  height: 6rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10rem;
+  margin-bottom: 3rem;
+  gap: 1rem;
+
+  div {
+    display: flex;
+    gap: 2rem;
+  }
+`;
+
+export const DescriptionTitle = styled.h2`
+  /* width: 100%; */
+  font-size: 1.5rem;
+  color: #000000;
+  font-weight: 500;
+`;
+
+export const AddInformationTitle = styled.h3`
+  /* width: 100%; */
+  font-size: 1.5rem;
+  color: #9f9f9f;
+  font-weight: 400;
+`;
+
+export const Description = styled.p`
+  width: 64rem;
+  color: #9f9f9f;
+`;
